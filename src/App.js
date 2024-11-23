6aac00faaf20ed65f3e0b732f2894a6e82a9b794
CanerYesiltas <caneryesiltas1@gmail.com>
2024-11-23 13:03:40 -0500

    update routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import AppRouter from './router/AppRouter';
 import './styles/App.css';
+import theme from './components/globalStyles/theme';
+import { ThemeProvider } from 'styled-components';
 
 function App() {
-  return <div>
-    <AppRouter/>
-  </div>;
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <AppRouter />
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,12 @@
 import { GlobalStyles } from '../components/globalStyles/Global.styles';
-import Navbar from '../components/navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Navbar from '../components/navbar/Navbar';
 import Login from '../pages/login/Login';
 import About from '../pages/about/About';
 import Home from '../pages/home/Home';
+import PrivateRouter from './PrivateRouter';
+import Detail from '../components/detail/Detail';
 
 const AppRouter = () => {
   return (
@@ -12,9 +14,18 @@ const AppRouter = () => {
       <GlobalStyles />
       <Navbar />
       <Routes>
+
         <Route path='/' element={<Home />} />
         <Route path='login' element={<Login />} />
-        <Route path='about' element={<About />} />
+
+        <Route path='about' element={<PrivateRouter/>} >
+        <Route path='' element={<About/>} />
+        </Route>
+
+<Route  path="detail" element={<PrivateRouter/>}  >
+  <Route path="" element={<Detail/>}  />
+</Route>
+
       </Routes>
     </BrowserRouter>
   );
