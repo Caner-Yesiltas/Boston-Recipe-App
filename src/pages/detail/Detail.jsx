f84e927e7211156d96707ff40b806c69f3bc9317
CanerYesiltas <caneryesiltas1@gmail.com>
2024-11-25 14:10:44 -0500

    add api key, id and async await structure at home page

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Detail = () => {
+  
   return <div>Detail</div>;
 };
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,25 @@
+import { useEffect } from 'react';
 import HomeStyle from './Home.style'
+import axios from "axios"
 
 const Home = () => {
+  const APP_ID = "0a8f3cf7";
+  const APP_KEY = "71b3096aed8981f4c337f35fe72e1218";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+
+  const getData = async () => {
+    
+  const {data}= await axios.get(url)
+  }
+
+  useEffect(() => {
+   
+  getData();
+   
+  }, [])
+  
+
   return (
     <div>
       
