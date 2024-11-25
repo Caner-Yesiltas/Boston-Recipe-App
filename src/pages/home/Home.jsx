fb1666018ca6ab30c0bb959185ee3928f4c6dd89
CanerYesiltas <caneryesiltas1@gmail.com>
2024-11-25 17:56:25 -0500

    add try catch

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,30 +1,37 @@
-import { useEffect } from 'react';
-import HomeStyle from './Home.style'
-import axios from "axios"
+import { useEffect, useState } from 'react';
+import HomeStyle from './Home.style';
+import axios from 'axios';
 
 const Home = () => {
-  const APP_ID = "0a8f3cf7";
-  const APP_KEY = "71b3096aed8981f4c337f35fe72e1218";
+  const [query, setQuery] = useState('egg');
+  const [selectedMeal, setSelectedMeal] = useState('breakfast');
+  const [recipe, setRecipes] = useState('');
 
-  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+  const APP_ID = '0a8f3cf7';
+  const APP_KEY = '71b3096aed8981f4c337f35fe72e1218';
 
-  const getData = async () => {
-    
-  const {data}= await axios.get(url)
-  }
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealtype=${selectedMeal}`;
 
-  useEffect(() => {
-   
-  getData();
-   
-  }, [])
+
   
 
-  return (
-    <div>
+  const getData = async () => {
+    try {
+      const { data } = await axios.get(url);
+      setRecipes(data.hits)
+    } catch (error) {
+      console.log(error);
       
-    </div>
-  )
-}
+    }
 
+   
+  };
+
+  
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <div></div>;
+};  
 export default Home;
